486e9f7e78ea42ec6d56aeba0006cb24b3e4b361
MohamedAspar <114578242+MohamedAspar@users.noreply.github.com>
2023-07-04 09:58:28 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,15 @@
+def serious(a):
+    x=[];
+    for i in range(a):
+        if a == 1 or a ==2:
+            x.append(1)
+        else:
+            x.append(2*i +1)
+
+    print(x)
+
+a=5
+serious(a)
+
+
+
